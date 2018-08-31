49c84b57deabffb5be15530bd40d903ae12de4cf
Jan Ouwens <jan.ouwens@gmail.com>
2018-08-31 09:38:19 +0200

    Zsh: makes an alias from cat to bat

--- zsh/scripts/environment/aliases.sh
+++ zsh/scripts/environment/aliases.sh
@@ -1,3 +1,6 @@
+# brew install bat
+# brew install ctags
+
 alias cd..='cd ..'
 alias vanilla-vim="vim -u NONE -U NONE -N"
 
@@ -8,7 +11,10 @@ alias gst='git status'
 # So we can type `git add *Test*` instead of `git add "*Test*"`
 alias git="noglob git"
 
+# `bat`: a better `cat`
+alias cat=bat
 
+# MacOS pre-installs a bad version of ctags so we substitute our own.
 if [[ "$(uname -s)" == "Darwin" ]]
 then
   alias ctags="`brew --prefix`/bin/ctags"
