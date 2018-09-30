2b09c66730478084353e8c2a6993dff839515f39
Jan Ouwens <jan.ouwens@gmail.com>
2018-09-30 09:06:21 +0200

    Updates backup scripts

--- zsh/scripts/scripts/backup-docs.sh
+++ zsh/scripts/scripts/backup-docs.sh
@@ -1,3 +1,4 @@
 
-rsync --progress -e "ssh -c aes256-cbc" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude "._*" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "@eaDir" /Volumes/EXTERNAL/documents/* jqno@192.168.0.103:/volume1/documents
+# rsync --progress -e "ssh -c aes256-cbc" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude "._*" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "@eaDir" /Volumes/EXTERNAL/documents/* jqno@192.168.0.103:/volume1/documents
+rsync --progress -e "ssh -c aes256-cbc" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude "._*" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "@eaDir" /Volumes/EXTERNAL/documents/* jqno@dekluis:/volume1/documents
 
