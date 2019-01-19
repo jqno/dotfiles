26a1bd9d23356ac64a88396035a9fc6a3e3e31db
Jan Ouwens <jan.ouwens@gmail.com>
2019-01-19 10:26:15 +0100

    Hammerspoon: replaces official Whatsapp app with a thrid-party one that sucks less

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -31,7 +31,7 @@ hyper.bindKey({}, "\\", function() am.switchToAndFromApp("com.hicknhacksoftware.
 hyper.bindKey({}, "s", function() am.switchToAndFromApp("com.spotify.client") end)
 hyper.bindKey({}, "v", function() am.switchToAndFromApp("org.vim.MacVim") end)
 hyper.bindKey({}, "w", function() am.switchToAndFromApp("org.mozilla.firefox") end)
-hyper.bindKey({}, "1", function() am.switchToAndFromApp("desktop.WhatsApp") end)
+hyper.bindKey({}, "1", function() am.switchToAndFromApp("com.rockysandstudio.app-for-whatsapp") end)
 hyper.bindKey({}, "2", function() am.switchToAndFromApp("com.tinyspeck.slackmacgap") end)
 hyper.bindKey({}, "3", function() am.switchToAndFromApp("chat.rocket") end)
 
@@ -71,7 +71,7 @@ local layouts = {
   { bundle = "com.grupovrs.ramboxce", func = function(win) wm.windowMaximize(0, win) end },
   { bundle = "com.spotify.client", func = function(win) wm.windowMaximize(0, win) end },
   { bundle = "com.tinyspeck.slackmacgap", func = function(win) wm.windowMaximize(0, win) end },
-  { bundle = "desktop.WhatsApp", func = function(win) wm.windowMaximize(0, win) end },
+  { bundle = "com.rockysandstudio.app-for-whatsapp", func = function(win) wm.windowMaximize(0, win) end },
   { bundle = "it.bloop.airmail2", func = function(win) wm.windowMaximize(1, win) end },
   { bundle = "org.keepassx.keepassxc", func = function(win) wm.windowMaximize(2, win) end },
   { bundle = "org.mozilla.firefox", func = function(win) wm.windowMaximize(0, win) end }
