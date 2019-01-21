b39cd92fa54f8cb198f9be9927db695ec6bd54c8
Jan Ouwens <jan.ouwens@gmail.com>
2019-01-21 15:05:07 +0100

    Hammerspoon: better layouting for communications apps

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -62,16 +62,15 @@ hyper.bindKey({"cmd"}, "k", function() wm.fw():moveOneScreenNorth() end)
 hyper.bindKey({"cmd"}, "l", function() wm.fw():moveOneScreenEast() end)
 
 local layouts = {
-  { bundle = "chat.rocket", func = function(win) wm.windowMaximize(0, win) end },
+  { bundle = "chat.rocket", func = function(win) wm.windowMaximize(1, win) end },
   { bundle = "com.apple.iCal", func = function(win) wm.windowMaximize(1, win) end },
   { bundle = "com.apple.Safari", func = function(win) wm.windowMaximize(1, win) end },
   { bundle = "com.google.Chrome", func = function(win) wm.windowMaximize(1, win) end },
   { bundle = "com.hicknhacksoftware.MacPass", func = function(win) wm.windowMaximize(2, win) end },
   { bundle = "com.jetbrains.intellij", func = function(win) wm.windowMaximize(0, win) end },
-  { bundle = "com.grupovrs.ramboxce", func = function(win) wm.windowMaximize(0, win) end },
   { bundle = "com.spotify.client", func = function(win) wm.windowMaximize(0, win) end },
-  { bundle = "com.tinyspeck.slackmacgap", func = function(win) wm.windowMaximize(0, win) end },
-  { bundle = "com.rockysandstudio.app-for-whatsapp", func = function(win) wm.windowMaximize(0, win) end },
+  { bundle = "com.tinyspeck.slackmacgap", func = function(win) wm.windowMaximize(1, win) end },
+  { bundle = "com.rockysandstudio.app-for-whatsapp", func = function(win) wm.windowMaximize(1, win) end },
   { bundle = "it.bloop.airmail2", func = function(win) wm.windowMaximize(1, win) end },
   { bundle = "org.keepassx.keepassxc", func = function(win) wm.windowMaximize(2, win) end },
   { bundle = "org.mozilla.firefox", func = function(win) wm.windowMaximize(0, win) end }
