adb35c62d929d830fe7cd09af852f824483b4505
Jan Ouwens <jan.ouwens@gmail.com>
2019-02-12 15:26:32 +0100

    Hammerspoon: maximize iTerm windows: we're going to do splits

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -22,7 +22,7 @@ hyper.bindKey({"cmd"}, "r", hs.reload)
 
 -- App switching
 hyper.bindKey({}, "space", function() am.switchToAndFromApp("com.googlecode.iterm2") end)
-hyper.bindKey({"cmd"}, "space", function() am.newItermWindow() end)
+hyper.bindKey({"cmd"}, "space", function() wm.windowMaximize(0, am.newItermWindow()) end)
 hyper.bindKey({}, "c", function() am.switchToAndFromApp("com.apple.iCal") end)
 hyper.bindKey({}, "d", function() am.switchToAndFromApp("com.kapeli.dashdoc") end)
 hyper.bindKey({}, "f", function() am.switchToAndFromApp("com.apple.finder") end)
@@ -66,6 +66,7 @@ local layouts = {
   { bundle = "chat.rocket", func = function(win) wm.windowMaximize(1, win) end },
   { bundle = "com.apple.iCal", func = function(win) wm.windowMaximize(1, win) end },
   { bundle = "com.apple.Safari", func = function(win) wm.windowMaximize(1, win) end },
+  { bundle = "com.googlecode.iterm2", func = function(win) wm.windowMaximize(0, win) end },
   { bundle = "com.google.Chrome", func = function(win) wm.windowMaximize(1, win) end },
   { bundle = "com.hicknhacksoftware.MacPass", func = function(win) wm.windowMaximize(2, win) end },
   { bundle = "com.jetbrains.intellij", func = function(win) wm.windowMaximize(0, win) end },
