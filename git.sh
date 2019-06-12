61109b324463d2b3fa166cc186335cc5fb4efc1d
Jan Ouwens <jan.ouwens@gmail.com>
2019-06-12 13:17:37 +0200

    Vim/Git: adds diffconflicts plugin

--- git.sh
+++ git.sh
@@ -51,8 +51,10 @@ git config --global alias.unassume "update-index --no-assume-unchanged"
 git config --global alias.assumed "!git ls-files -v | grep ^h | cut -c 3-"
 
 
-git config --global mergetool.vim.cmd "vim -d \$LOCAL \$BASE \$REMOTE \$MERGED -c '\$wincmd w' -c 'wincmd J'"
-git config --global merge.tool vim
+git config --global merge.tool vimdiffconflicts
+git config --global mergetool.vimdiffconflicts.cmd 'vim -c DiffConflicts "$MERGED" "$BASE" "$LOCAL" "$REMOTE"'
+git config --global mergetool.vimdiffconflicts.trustExitCode true
+git config --global mergetool.keepBackup false
 git config --global merge.conflictstyle diff3
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
