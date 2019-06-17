0cb02f2eca8d4b469a403fb8a3a2e2e4b1637ca7
Jan Ouwens <jan.ouwens@gmail.com>
2019-06-17 16:53:57 +0200

    Zsh: explicitly sources config scripts

--- scripts/run-all-scripts-in-dir.sh
+++
@@ -1,9 +0,0 @@
-# Add the following line to ~/.bashrc or ~/.zshrc:
-# . /path/to/this/script/run.sh script_folder
-# For example:
-# . ./run-all-scripts-in-dir.sh ~/.zsh/scripts
-# Don't forget the dot!
-
-for f in $1/*; do
-   . $f
-done
