41ececcc4e81dc8fbb5da77f760d3beaae27a94a
Jan Ouwens <jan.ouwens@gmail.com>
2019-06-25 15:52:02 +0200

    Vim: fixes a few issues with plugin lockfile

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ function installDotfiles() {
   echo "Installing Vim plugins..."
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  vim -c JqnoPlugInstall
+  vim -c JqnoPlugInitialInstall
 }
 
 
