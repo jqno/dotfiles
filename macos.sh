f262a93b48e3dfbee22f084740551c888fbe4d65
Jan Ouwens <jan.ouwens@gmail.com>
2019-08-28 08:49:10 +0200

    macOS: don't force quit Terminal, because that's probably running this script

--- macos.sh
+++ macos.sh
@@ -58,8 +58,7 @@ if [ "$(uname -s)" == "Darwin" ]; then
     "Finder" \
     "Photos" \
     "Safari" \
-    "SystemUIServer" \
-    "Terminal"; do
+    "SystemUIServer"; do
     killall "${app}" &> /dev/null
   done
   echo "Done. Note that some of these changes require a logout/restart to take effect."
