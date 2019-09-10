f5a9858b72a4ac37815d764577f200aec46c19a6
Jan Ouwens <jan.ouwens@gmail.com>
2019-09-10 15:16:18 +0200

    Scripts: adds fallback hosts for NAS

--- scripts/backup-photos.sh
+++ scripts/backup-photos.sh
@@ -5,7 +5,6 @@ else
   FOLDER=$1
 fi
 
-# rsync --dry-run -e "ssh -c aes256-cbc" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "*.photoslibrary" --exclude "@eaDir" /Volumes/Backup/photos/$FOLDER jqno@dekluis:/volume1/photos
-# rsync --progress -e "ssh -c aes256-cbc" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "*.photoslibrary" --exclude "@eaDir" /Volumes/Backup/photos/$FOLDER jqno@192.168.0.103:/volume1/photos
 rsync --progress -e "ssh -c aes256-cbc" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "*.photoslibrary" --exclude "@eaDir" /Volumes/Backup/photos/$FOLDER jqno@dekluis:/volume1/photos
+# rsync --progress -e "ssh -c aes256-cbc" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "*.photoslibrary" --exclude "@eaDir" /Volumes/Backup/photos/$FOLDER jqno@192.168.178.30:/volume1/photos
 
