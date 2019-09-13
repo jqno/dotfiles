a5d5f8eb918eab45d92ff649de7220441c40e6cb
Jan Ouwens <jan.ouwens@gmail.com>
2019-09-13 15:17:08 +0200

    Zsh: fixes whitespace in aliases.sh

--- zsh/config/aliases.sh
+++ zsh/config/aliases.sh
@@ -37,17 +37,17 @@ then
     java -version
   }
   function setjdk-silent() {
-    if [ $# -ne 0 ]; then  
-     removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'  
-     if [ -n "${JAVA_HOME+x}" ]; then  
-      removeFromPath $JAVA_HOME  
-     fi  
-     export JAVA_HOME=`/usr/libexec/java_home -v $@`
-     export PATH=$JAVA_HOME/bin:$PATH  
-    fi  
-  }  
-  function removeFromPath() {  
-    export PATH=$(echo $PATH | sed -E -e "s;:$1;;" -e "s;$1:?;;")  
+    if [ $# -ne 0 ]; then
+      removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'
+      if [ -n "${JAVA_HOME+x}" ]; then
+        removeFromPath $JAVA_HOME
+      fi
+      export JAVA_HOME=`/usr/libexec/java_home -v $@`
+      export PATH=$JAVA_HOME/bin:$PATH
+    fi
+  }
+  function removeFromPath() {
+    export PATH=$(echo $PATH | sed -E -e "s;:$1;;" -e "s;$1:?;;")
   }
 
   setjdk-silent 11
