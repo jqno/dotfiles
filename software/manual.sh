c4a136d6959fe55b7ccc9604812715c60680ab80
Jan Ouwens <jan.ouwens@gmail.com>
2019-11-12 11:20:03 +0100

    Install.sh: adds Scala Metals to manual software

--- software/manual.sh
+++ software/manual.sh
@@ -15,3 +15,16 @@ tar xf archive.tar.gz
 rm archive.tar.gz
 popd > /dev/null
 
+# Scala Metals Language Server
+echo "** Installing Scala Metals Language Server"
+curl -L -o "$BIN/coursier" https://git.io/coursier
+chmod +x "$BIN/coursier"
+coursier bootstrap \
+  --java-opt -Xss4m \
+  --java-opt -Xms100m \
+  --java-opt -Dmetals.client=vim-lsc \
+  org.scalameta:metals_2.12:0.7.6 \
+  -r bintray:scalacenter/releases \
+  -r sonatype:snapshots \
+  -o /usr/local/bin/metals-vim -f
+
