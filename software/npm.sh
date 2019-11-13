e5955d3589234efc865770e38f8434e57b264e9e
Jan Ouwens <jan.ouwens@gmail.com>
2019-11-13 15:09:43 +0100

    Vim: adds Vim language server

--- software/npm.sh
+++ software/npm.sh
@@ -5,5 +5,6 @@ npm install --global chokidar chokidar-cli
 npm install --global elm elm-format elm-test
 npm install --global @elm-tooling/elm-language-server
 npm install --global typescript typescript-language-server
+npm install --global vim-language-server
 npm install --global webpack webpack-cli
 
