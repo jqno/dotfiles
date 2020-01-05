260b0cce15301ab3de9901c562b14fff52f32a1c
Jan Ouwens <jan.ouwens@gmail.com>
2020-01-05 19:19:32 +0100

    Script: simplifies falling back to server's IP address

--- scripts/backup-photos.sh
+++ scripts/backup-photos.sh
@@ -5,6 +5,8 @@ else
   FOLDER=$1
 fi
 
-rsync --progress -e "ssh -c aes256-cbc" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "*.photoslibrary" --exclude "@eaDir" /Volumes/Backup/photos/$FOLDER jqno@dekluis:/volume1/photos
-# rsync --progress -e "ssh -c aes256-cbc" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "*.photoslibrary" --exclude "@eaDir" /Volumes/Backup/photos/$FOLDER jqno@192.168.178.30:/volume1/photos
+SERVER="dekluis"
+# SERVER="192.168.178.30"
+
+rsync --progress -e "ssh -c aes256-cbc" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "*.photoslibrary" --exclude "@eaDir" /Volumes/Backup/photos/$FOLDER jqno@$SERVER:/volume1/photos
 
