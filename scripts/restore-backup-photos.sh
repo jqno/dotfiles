dc1e8c5a020c122f8f06198a2fdf448dece92c3c
Jan Ouwens <jan.ouwens@gmail.com>
2020-02-21 11:30:09 +0100

    Script: adds script to restore a backup from NAS

---
+++ scripts/restore-backup-photos.sh
@@ -0,0 +1,5 @@
+SERVER="dekluis"
+# SERVER="192.168.178.30"
+
+rsync --progress -e "ssh -c aes256-cbc" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "*.photoslibrary" --exclude "@eaDir" jqno@$SERVER:/volume1/photos /Volumes/Backup
+
