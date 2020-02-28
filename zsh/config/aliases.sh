3b3e73367550fa4f50befe36a5a6570c215c2908
Jan Ouwens <jan.ouwens@gmail.com>
2020-02-28 08:49:50 +0100

    Zsh: always show Java version in prompt

--- zsh/config/aliases.sh
+++ zsh/config/aliases.sh
@@ -41,20 +41,6 @@ then
   function setjdk() {
     setjdk-silent $@
     java -version
-
-    JAVA_VERSION="$(java -version 2>&1 | grep 'version' | sed -E 's/.*version "(.*)".*/\1/')"
-    if [[ $JAVA_VERSION == *"."* ]]; then
-      if [[ $JAVA_VERSION == "1."* ]]; then
-        # Java 8 and lower (1.8.*)
-        export JAVA_MAJOR_VERSION=$(echo $JAVA_VERSION | sed "s/1\.//" | sed "s/\..*//" )
-      else
-        # Java 9 and up (9.*)
-        export JAVA_MAJOR_VERSION=$(echo $JAVA_VERSION | sed "s/\..*//" )
-      fi
-    else
-      # Early Access versions (don't contain .)
-      export JAVA_MAJOR_VERSION=$JAVA_VERSION
-    fi
   }
 
   function setjdk-silent() {
@@ -65,9 +51,23 @@ then
       fi
       export JAVA_HOME=`/usr/libexec/java_home -v $@`
       export PATH=$JAVA_HOME/bin:$PATH
-      export JAVA_MAJOR_VERSION=""
+
+      JAVA_VERSION="$(java -version 2>&1 | grep 'version' | sed -E 's/.*version "(.*)".*/\1/')"
+      if [[ $JAVA_VERSION == *"."* ]]; then
+        if [[ $JAVA_VERSION == "1."* ]]; then
+          # Java 8 and lower (1.8.*)
+          export JAVA_MAJOR_VERSION=$(echo $JAVA_VERSION | sed "s/1\.//" | sed "s/\..*//" )
+        else
+          # Java 9 and up (9.*)
+          export JAVA_MAJOR_VERSION=$(echo $JAVA_VERSION | sed "s/\..*//" )
+        fi
+      else
+        # Early Access versions (don't contain .)
+        export JAVA_MAJOR_VERSION=$JAVA_VERSION
+      fi
     fi
   }
+
   function removeFromPath() {
     export PATH=$(echo $PATH | sed -E -e "s;:$1;;" -e "s;$1:?;;")
   }
