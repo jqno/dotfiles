3740f54b5c13546dd3007892eb776f88ac277c3d
Jan Ouwens <jan.ouwens@gmail.com>
2020-07-13 09:44:17 +0200

    Software: adds bundler

--- software/gem.sh
+++ software/gem.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # Built-in gem won't install globally, and brew refuses to replace its version with the built-in one: let's call it directly.
+/usr/local/opt/ruby/bin/gem install bundler
 /usr/local/opt/ruby/bin/gem install jekyll
 /usr/local/opt/ruby/bin/gem install github-pages
 
