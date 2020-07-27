0492529795ca5ff607ceafe8aa9e256910645120
Jan Ouwens <jan.ouwens@gmail.com>
2020-07-27 15:30:10 +0200

    Script: updates scala-organize-imports

--- scripts/scala-organize-imports.sh
+++ scripts/scala-organize-imports.sh
@@ -4,7 +4,7 @@ echo "Organizing $@"
 echo "Please wait..."
 
 scalafix \
-  --tool-classpath $(cs fetch com.github.liancheng:organize-imports_2.13:0.3.1-RC3 -p) \
+  --tool-classpath $(cs fetch com.github.liancheng:organize-imports_2.13:0.4.0 -p) \
   --scalac-options -Ywarn-unused \
   --rules OrganizeImports \
   --classpath $(mvn -q org.codehaus.mojo:exec-maven-plugin:exec -Dexec.classpathScope="compile" -Dexec.executable="echo" -Dexec.args="%classpath") \
