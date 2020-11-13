bf3c53d19a236d3b1d6871b1579c39bac777464f
Jan Ouwens <jan.ouwens@gmail.com>
2020-11-13 15:27:09 +0100

    Zsh: makes sure $PATH is only set once

--- zsh/config/environment.sh
+++ zsh/config/environment.sh
@@ -1,6 +1,9 @@
 # PATH
-GEMDIR=`gem env gemdir`/bin
-export PATH=/usr/local/sbin:/sbin:~/bin:~/scripts:~/Library/Application\ Support/Coursier/bin:~/.cargo/bin:~/Library/Python/3.7/bin:/usr/local/opt/ruby/bin:$GEMDIR:$PATH
+if [ -z ${ZSH_ENV_LOADED+x} ]; then
+  ZSH_ENV_LOADED=1
+  GEMDIR=`gem env gemdir`/bin
+  export PATH=/usr/local/sbin:/sbin:~/bin:~/scripts:~/Library/Application\ Support/Coursier/bin:~/.cargo/bin:~/Library/Python/3.7/bin:/usr/local/opt/ruby/bin:$GEMDIR:$PATH
+fi
 
 # Default editor
 export EDITOR=vim
