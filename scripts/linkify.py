5a4560d1848e557afbcc65d33b51a4acc5dd0eca
Jan Ouwens <jan.ouwens@gmail.com>
2021-01-13 15:48:36 +0100

    Vim: adds url-to-markdown-link mapping

---
+++ scripts/linkify.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+"""Fetches a page's <title> and turns it into a markdown link"""
+
+import sys
+from html.parser import HTMLParser
+import requests
+
+
+def main(url):
+    try:
+        resp = fetch(url)
+    except Exception as ex:
+        print(f"Failed: {ex}")
+        sys.exit(1)
+
+    if resp.ok:
+        title = extract_title(resp.text)
+        print(f"[{title}]({url})")
+    else:
+        print(f"Failed: {resp.status_code}")
+        sys.exit(1)
+
+
+def fetch(url):
+    headers = {'User-Agent': 'Googlebot/2.1 (+http://www.google.com/bot.html)'}
+    return requests.get(url, headers=headers)
+
+
+def extract_title(text):
+    class Parser(HTMLParser):
+        def __init__(self):
+            super().__init__()
+            self.active = False
+            self.title = ""
+
+        def handle_starttag(self, tag, attrs):
+            if tag == "title":
+                self.active = True
+
+        def handle_endtag(self, tag):
+            if tag == "title":
+                self.active = False
+
+        def handle_data(self, data):
+            if self.active:
+                self.title = data
+
+        def error(self, message):
+            print(f"Error: {message}")
+            sys.exit(1)
+
+    parser = Parser()
+    parser.feed(text)
+    return parser.title
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Expected a url argument")
+        sys.exit(1)
+    main(sys.argv[1])
--- vim/ftplugin/markdown.vim
+++ vim/ftplugin/markdown.vim
@@ -1,3 +1,18 @@
 call SetTabstop(4)
 setlocal wrap
 
+function! s:Linkify() abort
+    let l:url = expand('<cWORD>')
+    let l:link = system('linkify.py ' . l:url)
+    let l:chomped = substitute(l:link, '\n\+$', '', '')
+    let l:prevchar = strpart(getline('.'), col('.') -2, 1)
+    if l:prevchar ==# ' ' || l:prevchar ==# ''
+        execute 'norm cW' . l:chomped
+    else
+        execute 'norm BcW' . l:chomped
+    endif
+endfunction
+
+command! Linkify call <SID>Linkify()
+nnoremap <leader>l :call <SID>Linkify()<CR>
+
