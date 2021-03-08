eedb395d4d50278790f2ace2d7107970b13904be
Jan Ouwens <jan.ouwens@gmail.com>
2021-03-08 11:07:30 +0100

    Script: remove obsolete backup scripts

--- scripts/backup-docs.sh
+++
@@ -1,5 +0,0 @@
-# SERVER="dekluis"
-SERVER="192.168.178.30"
-
-rsync --progress -e "ssh -c aes256-cbc -o IdentitiesOnly=yes" --iconv=UTF-8-MAC,UTF-8 --delete -av --exclude ".DS_Store" --exclude "._*" --exclude ".fseventsd" --exclude ".Spotlight-V100" --exclude ".TemporaryItems" --exclude ".Trashes" --exclude "@eaDir" /Volumes/files/documents/* jqno@$SERVER:/volume1/documents
-
