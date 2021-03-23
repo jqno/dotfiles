b4e8c47f97135d02cc06f8c286c7a84284e5fa2c
Jan Ouwens <jan.ouwens@gmail.com>
2021-03-23 14:07:10 +0100

    Neovim: splits configuration into several files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,1 @@
-
--- HELPERS --
-local function opt(scope, key, value)
-  scope[key] = value
-  if scope ~= vim.o then vim.o[key] = value end
-end
-
--- OPTIONS --
-local indent = 2
-opt(vim.bo, 'expandtab', true)
-opt(vim.bo, 'tabstop', indent)
-opt(vim.bo, 'shiftwidth', indent)
-opt(vim.bo, 'softtabstop', indent)
+require 'settings'
---
+++ nvim/lua/settings.lua
@@ -0,0 +1,12 @@
+-- HELPERS --
+local function set(scope, key, value)
+  scope[key] = value
+  if scope ~= vim.o then vim.o[key] = value end
+end
+
+-- SETTINGS --
+local indent = 2
+set(vim.bo, 'expandtab', true)
+set(vim.bo, 'tabstop', indent)
+set(vim.bo, 'shiftwidth', indent)
+set(vim.bo, 'softtabstop', indent)
