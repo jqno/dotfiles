c785be3b33de004aa49761a2cf921edd7730dc1f
Jan Ouwens <jan.ouwens@gmail.com>
2021-03-26 19:38:15 +0100

    Neovim: sets up mappings for Telescope

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,7 +7,14 @@ local function map(mode, lhs, rhs, opts)
   vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
+-- LEADER --
+vim.g.mapleader = ' '
+
 -- COMPLETION --
 map(modes.i, '<Tab>', 'v:lua.tab_complete()', { expr = true })
 map(modes.i, '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true })
 map(modes.i, '<CR>', 'compe#confirm("<CR>")', { expr = true })
+
+-- FINDING --
+map(modes.n, '<leader>ff', '<cmd>Telescope find_files<CR>')
+map(modes.n, '<leader>fg', '<cmd>Telescope live_grep<CR>')
