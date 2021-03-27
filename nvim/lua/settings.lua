3f7a9421dbdf4255b5463488c84f8d881f118624
Jan Ouwens <jan.ouwens@gmail.com>
2021-03-27 14:25:13 +0100

    Neovim: adds some convenient settings

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -7,9 +7,13 @@ end
 -- SETTINGS --
 local indent = 2
 set(vim.bo, 'expandtab', true)
-set(vim.bo, 'tabstop', indent)
 set(vim.bo, 'shiftwidth', indent)
 set(vim.bo, 'softtabstop', indent)
+set(vim.bo, 'tabstop', indent)
 
-set(vim.o, 'completeopt', 'menuone,noselect')
+set(vim.wo, 'number', true)
+set(vim.wo, 'relativenumber', true)
+set(vim.wo, 'signcolumn', 'yes')
+
+set(vim.o, 'completeopt', 'menuone,noselect,preview')
 set(vim.o, 'updatetime', 300)
