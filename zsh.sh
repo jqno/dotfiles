8169bf472721139739efdd4d27bf3a629055771e
Jan Ouwens <jan.ouwens@gmail.com>
2021-04-30 15:55:33 +0200

    Linux: makes zsh default shell on Linux

--- zsh.sh
+++ zsh.sh
@@ -5,6 +5,8 @@
 LOCATION=""
 if [ "$(uname -s)" == "Darwin" ]; then
   LOCATION="/usr/local/bin/zsh"
+elif [ "$(uname -s)" == "Linux" ]; then
+  LOCATION="/usr/bin/zsh"
 else
   echo "Don't know the location of zsh on this system: $(uname -s)"
   exit 1
