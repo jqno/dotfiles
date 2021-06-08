c439ab9e24734f45285dc7f8d293616df154f36f
Jan Ouwens <jan.ouwens@gmail.com>
2021-06-08 10:33:14 +0200

    Linux: configures CapsLock to be Hyper

--- install.sh
+++ install.sh
@@ -43,6 +43,7 @@ function installDotfiles() {
     ~/.yabairc install
   elif [[ "$OSNAME" == "Linux" ]]; then
     installFor "linux/abcde.conf" ".abcde.conf"
+    installFor "linux/Xmodmap" ".Xmodmap"
   fi
 }
 
