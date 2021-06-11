bb3e8fa79e270c637bc21a9a0245d9de747cb23b
Jan Ouwens <jan.ouwens@gmail.com>
2021-06-11 10:45:54 +0200

    Neovim: improves wildfire matching for html/xml

--- nvim/lua/plugin-settings.lua
+++ nvim/lua/plugin-settings.lua
@@ -167,7 +167,7 @@ end
 local function setup_wildfire()
   g.wildfire_objects = {
     ['scala'] = { 'iw', "i'", "a'", 'i"', 'a"', 'i)', 'i]', 'i}', 'ip' },
-    ['xml,xml.pom'] = { 'i}', 'a}', 'it', 'at' }
+    ['xml,xml.pom'] = { 'i}', 'a}', 'i"', 'a"', "i'", "a'", 'it', 'at' }
   }
 end
 
