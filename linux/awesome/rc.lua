c6b8bb33fe0e664ad80a75e9fee5ff8760931244
Jan Ouwens <jan.ouwens@gmail.com>
2021-06-20 09:43:25 +0200

    AwesomeWM: removes unused constant

--- linux/awesome/rc.lua
+++ linux/awesome/rc.lua
@@ -35,13 +35,6 @@ terminal = "kitty"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
-
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
     awful.layout.suit.floating,
