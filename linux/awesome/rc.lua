036c9c9524c5f51fd6a4251e4badd6a561607cba
Jan Ouwens <jan.ouwens@gmail.com>
2021-06-22 13:16:06 +0200

    AwesomeWM: removes strange wallpaper code

--- linux/awesome/rc.lua
+++ linux/awesome/rc.lua
@@ -42,25 +42,7 @@ mytextclock = wibox.widget.textclock()
 
 -- Create a wibox for each screen and add it
 
-local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
-    end
-end
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
-
 awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
-    set_wallpaper(s)
-
     -- Each screen has its own tag table.
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
