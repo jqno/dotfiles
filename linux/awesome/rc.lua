bfe30a3f830faaca3ae94fa46f73460fcc0f5d6e
Jan Ouwens <jan.ouwens@gmail.com>
2021-06-22 09:18:16 +0200

    AwesomeWM: makes new clients appear after the first instead of before

--- linux/awesome/rc.lua
+++ linux/awesome/rc.lua
@@ -209,7 +209,7 @@ awful.rules.rules = {
 client.connect_signal("manage", function (c)
     -- Set the windows at the slave,
     -- i.e. put it at the end of others instead of setting it master.
-    -- if not awesome.startup then awful.client.setslave(c) end
+    if not awesome.startup then awful.client.setslave(c) end
 
     if awesome.startup
       and not c.size_hints.user_position
