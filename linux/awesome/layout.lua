40111c2c2afe477adfeab7c1acdfdcc0c428058f
Jan Ouwens <jan.ouwens@gmail.com>
2021-06-22 09:00:30 +0200

    AwesomeWM: defines titlebars only for dialogs

--- linux/awesome/layout.lua
+++ linux/awesome/layout.lua
@@ -1,25 +1,51 @@
-local awful = require("awful")
-local beautiful = require("beautiful")
-local gears = require("gears")
+local awful = require('awful')
+local client = _G.client
+local beautiful = require('beautiful')
+local gears = require('gears')
+local wibox = require('wibox')
 
 local This = {}
+
+local function setup_layouts()
+  awful.layout.layouts = {
+    awful.layout.suit.tile,
+    awful.layout.suit.max,
+    awful.layout.suit.floating,
+    awful.layout.suit.magnifier
+  }
+end
 
 local function setup_theme()
   beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
 end
 
-local function setup_layouts()
-  awful.layout.layouts = {
-  awful.layout.suit.tile,
-  awful.layout.suit.max,
-  awful.layout.suit.floating,
-  awful.layout.suit.magnifier
-}
+local function setup_titlebars()
+  client.connect_signal('request::titlebars', function(c)
+    awful.titlebar(c):setup {
+      { -- Left
+        awful.titlebar.widget.iconwidget(c),
+        layout  = wibox.layout.fixed.horizontal
+      },
+      { -- Middle
+        {
+          align  = "center",
+          widget = awful.titlebar.widget.titlewidget(c)
+        },
+        layout  = wibox.layout.flex.horizontal
+      },
+      { -- Right
+        awful.titlebar.widget.closebutton(c),
+        layout = wibox.layout.fixed.horizontal()
+      },
+      layout = wibox.layout.align.horizontal
+    }
+  end)
 end
 
 function This.setup()
-  setup_theme()
   setup_layouts()
+  setup_theme()
+  setup_titlebars()
 end
 
 return This
--- linux/awesome/rc.lua
+++ linux/awesome/rc.lua
@@ -194,7 +194,7 @@ awful.rules.rules = {
       }, properties = { floating = true }},
 
     -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
+    { rule_any = {type = { "dialog" }
       }, properties = { titlebars_enabled = true }
     },
 
@@ -217,46 +217,6 @@ client.connect_signal("manage", function (c)
         -- Prevent clients from being unreachable after screen count changes.
         awful.placement.no_offscreen(c)
     end
-end)
-
--- Add a titlebar if titlebars_enabled is set to true in the rules.
-client.connect_signal("request::titlebars", function(c)
-    -- buttons for the titlebar
-    local buttons = gears.table.join(
-        awful.button({ }, 1, function()
-            c:emit_signal("request::activate", "titlebar", {raise = true})
-            awful.mouse.client.move(c)
-        end),
-        awful.button({ }, 3, function()
-            c:emit_signal("request::activate", "titlebar", {raise = true})
-            awful.mouse.client.resize(c)
-        end)
-    )
-
-    awful.titlebar(c) : setup {
-        { -- Left
-            awful.titlebar.widget.iconwidget(c),
-            buttons = buttons,
-            layout  = wibox.layout.fixed.horizontal
-        },
-        { -- Middle
-            { -- Title
-                align  = "center",
-                widget = awful.titlebar.widget.titlewidget(c)
-            },
-            buttons = buttons,
-            layout  = wibox.layout.flex.horizontal
-        },
-        { -- Right
-            awful.titlebar.widget.floatingbutton (c),
-            awful.titlebar.widget.maximizedbutton(c),
-            awful.titlebar.widget.stickybutton   (c),
-            awful.titlebar.widget.ontopbutton    (c),
-            awful.titlebar.widget.closebutton    (c),
-            layout = wibox.layout.fixed.horizontal()
-        },
-        layout = wibox.layout.align.horizontal
-    }
 end)
 
 mouse.enable_focus_follows_mouse()
