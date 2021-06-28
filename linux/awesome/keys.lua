5497d061c44cabcac65b15ff744eb9ec822ab1fa
Jan Ouwens <jan.ouwens@gmail.com>
2021-06-28 14:01:23 +0200

    AwesomeWM: improves code layout

--- linux/awesome/keys.lua
+++ linux/awesome/keys.lua
@@ -19,51 +19,51 @@ local function generate_general_keys(merge_with)
     merge_with,
 
     -- client
-    awful.key({ modkey, 'Shift' }, 'j', function () awful.client.swap.byidx(1) end,
+    awful.key({ modkey, 'Shift' }, 'j', function() awful.client.swap.byidx(1) end,
               { description = 'swap with next client by index', group = 'client' }),
-    awful.key({ modkey, 'Shift' }, 'k', function () awful.client.swap.byidx(-1) end,
+    awful.key({ modkey, 'Shift' }, 'k', function() awful.client.swap.byidx(-1) end,
               { description = 'swap with previous client by index', group = 'client' }),
     awful.key({ modkey }, 'Tab',
-              function ()
+              function()
                 awful.client.focus.history.previous()
                 if client.focus then
                   client.focus:raise()
                 end
               end,
               { description = 'go back', group = 'client' }),
-    awful.key({ modkey }, 'h', function () awful.client.focus.global_bydirection('left') end,
+    awful.key({ modkey }, 'h', function() awful.client.focus.global_bydirection('left') end,
               { description = 'focus left', group = 'client' }),
-    awful.key({ modkey }, 'j', function () awful.client.focus.global_bydirection('down') end,
+    awful.key({ modkey }, 'j', function() awful.client.focus.global_bydirection('down') end,
               { description = 'focus down', group = 'client' }),
-    awful.key({ modkey }, 'k', function () awful.client.focus.global_bydirection('up') end,
+    awful.key({ modkey }, 'k', function() awful.client.focus.global_bydirection('up') end,
               { description = 'focus up', group = 'client' }),
-    awful.key({ modkey }, 'l', function () awful.client.focus.global_bydirection('right') end,
+    awful.key({ modkey }, 'l', function() awful.client.focus.global_bydirection('right') end,
               { description = 'focus right', group = 'client' }),
     awful.key({ modkey }, 'u', awful.client.urgent.jumpto,
               { description = 'jump to urgent client', group = 'client' }),
 
     -- launcher
-    awful.key({ hyper }, 'Return', function () util.view_next(util.terminal) end,
+    awful.key({ hyper }, 'Return', function() util.view_next(util.terminal) end,
               { description = 'terminal', group = 'launcher' }),
-    awful.key({ modkey }, 'Return', function () awful.spawn(util.terminal) end,
+    awful.key({ modkey }, 'Return', function() awful.spawn(util.terminal) end,
               { description = 'open a terminal', group = 'launcher' }),
-    awful.key({ modkey }, 'space', function () awful.spawn('rofi -modi drun -show drun') end,
+    awful.key({ modkey }, 'space', function() awful.spawn('rofi -modi drun -show drun') end,
               { description = 'run prompt', group = 'launcher' }),
 
     -- layout
-    awful.key({ modkey, 'Control', 'Shift' }, 'space', function () awful.layout.inc(-1) end,
+    awful.key({ modkey, 'Control', 'Shift' }, 'space', function() awful.layout.inc(-1) end,
               { description = 'select previous', group = 'layout' }),
-    awful.key({ modkey, 'Control' }, 'space', function () awful.layout.inc(1) end,
+    awful.key({ modkey, 'Control' }, 'space', function() awful.layout.inc(1) end,
               { description = 'select next', group = 'layout' }),
-    awful.key({ modkey }, '=', function () awful.tag.incmwfact(-0.05) end,
+    awful.key({ modkey }, '=', function() awful.tag.incmwfact(-0.05) end,
               { description = 'decrease master width factor', group = 'layout' }),
-    awful.key({ modkey }, '-', function () awful.tag.incmwfact(0.05) end,
+    awful.key({ modkey }, '-', function() awful.tag.incmwfact(0.05) end,
               { description = 'increase master width factor', group = 'layout' }),
 
     -- screen
-    awful.key({ modkey, 'Control' }, 'j', function () awful.screen.focus_relative(1) end,
+    awful.key({ modkey, 'Control' }, 'j', function() awful.screen.focus_relative(1) end,
               { description = 'focus the next screen', group = 'screen' }),
-    awful.key({ modkey, 'Control' }, 'k', function () awful.screen.focus_relative(-1) end,
+    awful.key({ modkey, 'Control' }, 'k', function() awful.screen.focus_relative(-1) end,
               { description = 'focus the previous screen', group = 'screen' }),
 
     -- system
@@ -71,7 +71,7 @@ local function generate_general_keys(merge_with)
               { description = 'reload awesome', group = 'system' }),
     awful.key({ modkey }, 'BackSpace', function() util.run_script('lock.sh') end,
               { description = 'power menu', group = 'system' }),
-    awful.key({ modkey, "Shift" }, 'BackSpace', function() util.power_menu() end,
+    awful.key({ modkey, 'Shift' }, 'BackSpace', function() util.power_menu() end,
               { description = 'lock screen', group = 'system' }),
     awful.key({ modkey }, 'b', function() util.run(util.change_background) end,
               { description = 'change wallpaper', group = 'system' }),
