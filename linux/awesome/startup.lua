00394865bfeb7a64d30e8993d57654c101861e1b
Jan Ouwens <jan.ouwens@gmail.com>
2021-06-28 13:03:44 +0200

    AwesomeWM: fixes autostart scripts

--- linux/awesome/startup.lua
+++ linux/awesome/startup.lua
@@ -1,10 +1,13 @@
 local This = {}
 
-local awful = require('awful')
 local util = require('util')
 
+local autostart_scripts = {
+  util.script('display.sh'),
+  util.change_background
+}
+
 local autostart_apps = {
-  { util.script('display.sh'), '' },
   { 'nm-applet', '' },
   { 'blueman-applet', '' },
   { 'numlockx', 'on' },
@@ -14,13 +17,14 @@ local autostart_apps = {
   { 'rambox', '' },
   { 'teams', '' },
   { 'mailspring', '' },
-  { 'firefox', '' },
-  { util.change_background, '' }
+  { 'firefox', '' }
 }
 
 function This.setup()
-  for app = 1, #autostart_apps do
-    local cmd = autostart_apps[app]
+  for _, cmd in ipairs(autostart_scripts) do
+    util.run(cmd)
+  end
+  for _, cmd in ipairs(autostart_apps) do
     util.run_once(cmd[1], cmd[2])
   end
 end
