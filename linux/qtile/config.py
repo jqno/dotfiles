a2b04cb4ac76b24c51fbd43c389bb37366252e19
Jan Ouwens <jan.ouwens@gmail.com>
2021-07-10 14:02:56 +0200

    Qtile: defines layouts

--- linux/qtile/config.py
+++ linux/qtile/config.py
@@ -102,19 +102,12 @@ for i in groups:
     ])
 
 layouts = [
-    layout.Columns(border_focus_stack='#d75f5f'),
-    layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.Columns(
+        border_focus_stack = '#d75f5f',
+        margin = 4,
+        grow_amount = 5
+    ),
+    layout.Max()
 ]
 
 widget_defaults = dict(
