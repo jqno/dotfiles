5c3cf42a1f8dc4bd69db6a9ede2ed0e6bfab0115
Jan Ouwens <jan.ouwens@gmail.com>
2021-07-12 10:15:28 +0200

    Qtile: makes layout more consistent

--- linux/qtile/config.py
+++ linux/qtile/config.py
@@ -45,11 +45,16 @@ script_location = home + '/.config/qtile/scripts'
 
 keys = [
     # Switch between windows
-    Key([mod], 'h', lazy.layout.left(), desc='Move focus to left'),
-    Key([mod], 'l', lazy.layout.right(), desc='Move focus to right'),
-    Key([mod], 'j', lazy.layout.down(), desc='Move focus down'),
-    Key([mod], 'k', lazy.layout.up(), desc='Move focus up'),
-    Key([mod], 'o', lazy.layout.next(), desc='Move window focus to other window'),
+    Key([mod], 'h', lazy.layout.left(),
+        desc='Move focus to left'),
+    Key([mod], 'l', lazy.layout.right(),
+        desc='Move focus to right'),
+    Key([mod], 'j', lazy.layout.down(),
+        desc='Move focus down'),
+    Key([mod], 'k', lazy.layout.up(),
+        desc='Move focus up'),
+    Key([mod], 'o', lazy.layout.next(),
+        desc='Move window focus to other window'),
 
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
@@ -59,7 +64,8 @@ keys = [
         desc='Move window to the right'),
     Key([mod, 'shift'], 'j', lazy.layout.shuffle_down(),
         desc='Move window down'),
-    Key([mod, 'shift'], 'k', lazy.layout.shuffle_up(), desc='Move window up'),
+    Key([mod, 'shift'], 'k', lazy.layout.shuffle_up(),
+        desc='Move window up'),
 
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
@@ -70,7 +76,8 @@ keys = [
     Key([mod, 'control'], 'j', lazy.layout.grow_down(),
         desc='Grow window down'),
     Key([mod, 'control'], 'k', lazy.layout.grow_up(), desc='Grow window up'),
-    Key([mod], 'n', lazy.layout.normalize(), desc='Reset all window sizes'),
+    Key([mod], 'n', lazy.layout.normalize(),
+        desc='Reset all window sizes'),
 
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
@@ -78,15 +85,21 @@ keys = [
     # multiple stack panes
     Key([mod, 'shift'], 'Return', lazy.layout.toggle_split(),
         desc='Toggle between split and unsplit sides of stack'),
-    Key([mod], 'Return', lazy.spawn(terminal), desc='Launch terminal'),
+    Key([mod], 'Return', lazy.spawn(terminal),
+        desc='Launch terminal'),
 
     # Toggle between different layouts as defined below
-    Key([mod], 'Tab', lazy.next_layout(), desc='Toggle between layouts'),
-    Key([mod, 'shift'], 'q', lazy.window.kill(), desc='Kill focused window'),
+    Key([mod], 'Tab', lazy.next_layout(),
+        desc='Toggle between layouts'),
+    Key([mod, 'shift'], 'q', lazy.window.kill(),
+        desc='Kill focused window'),
 
-    Key([mod, 'control'], 'r', lazy.restart(), desc='Restart Qtile'),
-    Key([mod, 'shift'], 'BackSpace', lazy.shutdown(), desc='Shutdown Qtile'),
-    Key([mod], 'space', lazy.spawn('rofi -modi drun -show drun'), desc='Spawn an app using rofi'),
+    Key([mod, 'control'], 'r', lazy.restart(),
+        desc='Restart Qtile'),
+    Key([mod, 'shift'], 'BackSpace', lazy.shutdown(),
+        desc='Shutdown Qtile'),
+    Key([mod], 'space', lazy.spawn('rofi -modi drun -show drun'),
+        desc='Spawn an app using rofi'),
 
     # System keybindings
     Key([mod], 'BackSpace', lazy.spawn(script_location + '/lock.sh'), desc='Lock screen'),
