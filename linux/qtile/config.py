e1efc06f7a4192f28ab657f357997a2789d108fd
Jan Ouwens <jan.ouwens@gmail.com>
2021-07-16 09:34:37 +0200

    Qtile: defines windows that must remain floating

--- linux/qtile/config.py
+++ linux/qtile/config.py
@@ -347,11 +347,11 @@ cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
     *layout.Floating.default_float_rules,
-    Match(wm_class='confirmreset'),  # gitk
-    Match(wm_class='makebranch'),  # gitk
-    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='Arandr'),
+    Match(wm_class='Blueman-manager'),
+    Match(wm_class='Org.gnome.Characters'),
     Match(wm_class='ssh-askpass'),  # ssh-askpass
-    Match(title='branchdialog'),  # gitk
+    Match(title='Event Tester'), # xev
     Match(title='pinentry'),  # GPG key password entry
 ])
 auto_fullscreen = True
