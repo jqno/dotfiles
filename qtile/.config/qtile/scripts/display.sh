92cc02e68b7248a744e874d7b39546fe45426673
Jan Ouwens <jan.ouwens@gmail.com>
2021-08-27 12:39:44 +0200

    Qtile: fixes monitor designations

--- qtile/.config/qtile/scripts/display.sh
+++ qtile/.config/qtile/scripts/display.sh
@@ -9,7 +9,7 @@ if [[ "NUM_DISPLAYS" -eq 1 ]]; then
 else
   echo "Multi-monitor"
   xrandr \
-    --output DP-1 --off --output DP-2 --off \
+    --output DP1 --off --output DP2 --off \
     --output eDP1 --primary --mode 3840x2400 --pos 0x0 --scale 0.5x0.5 --rotate normal \
-    --output DP-3 --mode 1920x1080 --pos 1920x0 --rotate normal
+    --output DP3 --mode 1920x1080 --pos 1920x0 --rotate normal
 fi
