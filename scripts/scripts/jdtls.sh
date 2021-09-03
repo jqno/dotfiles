8e74abe7f6d34f767114fb249e3c6b713e633d2f
Jan Ouwens <jan.ouwens@gmail.com>
2021-09-03 08:54:36 +0200

    Neovim: fixes failing JDT.LS in projects using Lombok
    See https://github.com/redhat-developer/vscode-java/issues/652#issuecomment-466906125

--- scripts/scripts/jdtls.sh
+++ scripts/scripts/jdtls.sh
@@ -13,7 +13,6 @@ java \
     -Xms1g \
     -Xmx2G \
     -javaagent:$LOMBOK \
-    -Xbootclasspath/a:$LOMBOK \
     --add-modules=ALL-SYSTEM \
     --add-opens java.base/java.util=ALL-UNNAMED \
     --add-opens java.base/java.lang=ALL-UNNAMED \
