be2a99fde3dbff9a3800f5eef03d71aaff8445fa
Jan Ouwens <jan.ouwens@gmail.com>
2021-09-13 09:12:44 +0200

    Xkb: makes script extensible

--- qtile/.config/qtile/scripts/xkb.sh
+++ qtile/.config/qtile/scripts/xkb.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-mods="+mods(caps_mod3)+mods(compose)"
+setkeymap() {
+  mods=$1
+  device=""
+  [ "$2" ] && device="-i $2"
+  setxkbmap -print | sed "s/\(xkb_symbols.*\".*\)\(\".*\)/\1$mods\2/" | xkbcomp $device "-I$HOME/.config/xkb" - "$DISPLAY"
+}
 
-setxkbmap -print | sed "s/\(xkb_symbols.*\".*\)\(\".*\)/\1$mods\2/" | xkbcomp -I$HOME/.config/xkb - $DISPLAY
+mods="+mods(caps_mod3)+mods(compose)"
+setkeymap $mods
