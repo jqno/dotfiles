dc7692fdc915b46d6635e5e109247bfc78af259d
Jan Ouwens <jan.ouwens@gmail.com>
2021-09-13 12:49:42 +0200

    Xkb: debugging the Apple keyboard mapping

--- qtile/.config/qtile/scripts/xkb.sh
+++ qtile/.config/qtile/scripts/xkb.sh
@@ -3,14 +3,14 @@
 setkeymap() {
   mods=$1
   device_id=$2
-  mapping=$(setxkbmap -print | sed "s/\(xkb_symbols.*\".*\)\(\".*\)/\1$mods\2/")
+  regex="s/\(xkb_symbols.*\".*\)\(\".*\)/\1$mods\2/"
 
   if [ "$device_id" ]; then
     echo "Applying mappings for device $device_id"
-    echo "$mapping" | xkbcomp -i "$device_id" "-I$HOME/.config/xkb" - "$DISPLAY" > /dev/null 2>&1
+    setxkbmap -print | sed "$regex" | xkbcomp -i "$device_id" "-I$HOME/.config/xkb" - "$DISPLAY" > /dev/null 2>&1
   else
     echo "Applying mappings for all devices"
-    echo "$mapping" | xkbcomp "-I$HOME/.config/xkb" - "$DISPLAY" > /dev/null 2>&1
+    setxkbmap -print | sed "$regex" | xkbcomp "-I$HOME/.config/xkb" - "$DISPLAY" > /dev/null 2>&1
   fi
 }
 
@@ -24,6 +24,7 @@ mods="+mods(caps_mod3)+mods(compose)"
 setkeymap $mods
 
 # Set modifications for Apple keyboards
-apple_id=$(kbd_id "Apple")
-mods="+mods(caps_mod3)+mods(compose)+apple(swap_alt_super)"
+mods="+apple(swap_alt_super)+apple(compose)+mods(caps_mod3)"
+apple_id=$(kbd_id "Apple Inc.")
+[ "$apple_id" ] || apple_id=$(kbd_id "Jan Ouwens’s Keyboard")
 [ "$apple_id" ] && setkeymap "$mods" "$apple_id"
