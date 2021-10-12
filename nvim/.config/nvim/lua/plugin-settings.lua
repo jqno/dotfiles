de39f9cb1e9416c895338395fdcd9959c875f8a0
Jan Ouwens <jan.ouwens@gmail.com>
2021-10-12 09:56:23 +0200

    Neovim: makes telescope look a little nicer

--- nvim/.config/nvim/lua/plugin-settings.lua
+++ nvim/.config/nvim/lua/plugin-settings.lua
@@ -83,7 +83,7 @@ local function setup_telescope()
       selection_caret = '❯ ',
       path_display = function(_, path)
         local tail = require('telescope.utils').path_tail(path)
-        return string.format('%s   (%s)', tail, path)
+        return string.format(' %s · %s', tail, path)
       end
     }
   })
