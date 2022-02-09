5e08d1cf7ec43cd74022ad63dc7af90ccab3c826
Jan Ouwens <jan.ouwens@gmail.com>
2022-02-09 08:09:13 +0100

    Neovim: compacts empty directories in nvim-tree

--- nvim/.config/nvim/lua/plugin-settings.lua
+++ nvim/.config/nvim/lua/plugin-settings.lua
@@ -58,6 +58,7 @@ local function setup_nvim_tree()
     }
 
     g.nvim_tree_gitignore = 0
+    g.nvim_tree_group_empty = 1
     g.nvim_tree_show_icons = {git = 0, folders = 1}
     g.nvim_tree_quit_on_open = 1
 end
