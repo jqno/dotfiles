355ca4d44d3e198f811a259dcdcef2e091c24cfe
Jan Ouwens <jan.ouwens@gmail.com>
2022-02-25 08:31:28 +0100

    Neovim: fixes Lua warnings

--- nvim/.config/nvim/lua/filetypes/lua.lua
+++ nvim/.config/nvim/lua/filetypes/lua.lua
@@ -1,6 +1,8 @@
 local This = {}
 
-This.lsp_config = {Lua = {diagnostics = {globals = {'vim'}}}}
+This.lsp_config = {
+    Lua = {runtime = {version = 'LuaJIT'}, diagnostics = {globals = {'vim'}}}
+}
 
 function This.setup()
     require('util').set_buf_indent(4)
