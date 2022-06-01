1f85ac8de73fea4d12efe00c92eff9bb7e4a13c4
Jan Ouwens <jan.ouwens@gmail.com>
2022-06-01 15:19:14 +0200

    Software: adds Fedora with KDE

---
+++ os-specific/fedora-with-kde/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+echo "*** Some dependencies need the administrator password:"
+sudo -v
+
+echo "*** Installing dependencies..."
+
+
+# Terminal
+sudo dnf --assumeyes install kitty
+
+
+# System tools
+sudo yum --assumeyes groupinstall 'Development Tools'
+sudo dnf --assumeyes install gcc-g++
+sudo dnf --assumeyes install libxcrypt-compat
+sudo dnf --assumeyes install procps-ng
+sudo dnf --assumeyes install tlp
+sudo dnf --assumeyes install tlp-rdw
+
+
+# Fonts
+sudo dnf --assumeyes install fontawesome5-fonts-all
+sudo dnf --assumeyes install unifont-fonts
+
+
+# KDE
+sudo dnf --assumeyes copr enable capucho/bismuth 
+sudo dnf --assumeyes install bismuth
+sudo dnf --assumeyes install kvantum
