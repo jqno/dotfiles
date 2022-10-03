834d5f8a1deae29cbf8c7002f4e659960d7fc9a9
Jan Ouwens <jan.ouwens@gmail.com>
2022-10-03 12:35:34 +0200

    Neovim: clear terminal before sending something to the terminal

--- nvim/.config/nvim/lua/util.lua
+++ nvim/.config/nvim/lua/util.lua
@@ -44,6 +44,7 @@ function This.floatermsend(cmd)
         vim.cmd('FloatermNew --silent')
     end
     vim.cmd('FloatermShow')
+    vim.cmd('FloatermSend clear')
     vim.cmd('FloatermSend ' .. cmd)
 end
 
