f047f7de7f5864f32c55d9ea228353b413eb54fb
Jan Ouwens <jan.ouwens@gmail.com>
2022-10-25 15:39:43 +0200

    Neovim: improves configuration script

--- configure/nvim.sh
+++ configure/nvim.sh
@@ -2,12 +2,17 @@
 
 rm -rf ~/.local/share/nvim
 rm -rf ~/.cache/nvim
+mkdir ~/.cache/nvim
 
-echo "*** Installing Neovim plugins..."
+PLUG_VIM="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c "curl -fLo $PLUG_VIM --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
-nvim --headless +PlugInstall +qall
-nvim --headless +PlugRevert +qall
+echo "*** Installing Neovim plugins... this may take a while..."
 
+nvim --headless \
+  -u "$HOME"/.config/nvim/plugins.vim \
+  -c PlugInstall \
+  -S "$HOME"/.config/nvim/plugin.lock.vim \
+  -c qall
