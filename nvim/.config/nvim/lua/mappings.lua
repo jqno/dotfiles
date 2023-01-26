5671684a45d148c2cf35de4e54961ae155b7f562
Jan Ouwens <jan.ouwens@gmail.com>
2023-01-26 19:06:55 +0100

    Neovim: replaces " with '

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -30,11 +30,11 @@ local function define_mappings()
     map(This.modes.n, 'N', 'Nzz')
     -- Toggle movements
     map(This.modes.n, '0',
-        function() require("util").toggle_movement("^", "0") end, { desc = 'toggle movement 0' })
+        function() require('util').toggle_movement('^', '0') end, { desc = 'toggle movement 0' })
     map(This.modes.n, ';',
-        function() require("util").toggle_movement(";", "0;") end, { desc = 'toggle movement ;' })
+        function() require('util').toggle_movement(';', '0;') end, { desc = 'toggle movement ;' })
     map(This.modes.n, ',',
-        function() require("util").toggle_movement(",", "$,") end, { desc = 'toggle movement ,' })
+        function() require('util').toggle_movement(',', '$,') end, { desc = 'toggle movement ,' })
     -- Breakpoints for undo
     map(This.modes.i, '.', '.<C-G>u')
     map(This.modes.i, ',', ',<C-G>u')
@@ -54,7 +54,7 @@ local function define_mappings()
     map(This.modes.v, '<M-k>', [[:move '<-2<CR>gv=gv]])
 
     -- Close everything --
-    map(This.modes.n, '<C-Esc>', function() require("util").close_everything() end,
+    map(This.modes.n, '<C-Esc>', function() require('util').close_everything() end,
         { desc = 'Close everything' })
     map(This.modes.t, '<C-Esc>', '<cmd>FloatermHide<CR>', { desc = 'Close everything' })
 
@@ -92,7 +92,7 @@ local function define_mappings()
     -- NAVIGATION --
     map(This.modes.n, '<leader><leader><leader>', function() require('harpoon.ui').toggle_quick_menu() end,
         { desc = 'open Harpoon quick list' })
-    map(This.modes.n, '<leader><leader><CR>', function() require("util").open_alternate() end,
+    map(This.modes.n, '<leader><leader><CR>', function() require('util').open_alternate() end,
         { desc = 'open alternate here' })
     map(This.modes.n, '<leader><leader>1', function() require('harpoon.ui').nav_file(1) end,
         { desc = 'navigate to Harpoon file #1' })
@@ -104,43 +104,43 @@ local function define_mappings()
         { desc = 'navigate to Harpoon file #4' })
     map(This.modes.n, '<leader><leader>5', function() require('harpoon.ui').nav_file(5) end,
         { desc = 'navigate to Harpoon file #5' })
-    map(This.modes.n, '<leader><leader>h', function() require("util").open_split("left") end,
+    map(This.modes.n, '<leader><leader>h', function() require('util').open_split('left') end,
         { desc = 'open split left' })
-    map(This.modes.n, '<leader><leader>j', function() require("util").open_split("down") end,
+    map(This.modes.n, '<leader><leader>j', function() require('util').open_split('down') end,
         { desc = 'open split below' })
-    map(This.modes.n, '<leader><leader>k', function() require("util").open_split("up") end,
+    map(This.modes.n, '<leader><leader>k', function() require('util').open_split('up') end,
         { desc = 'open split above' })
-    map(This.modes.n, '<leader><leader>l', function() require("util").open_split("right") end,
+    map(This.modes.n, '<leader><leader>l', function() require('util').open_split('right') end,
         { desc = 'open split right' })
     map(This.modes.n, '<leader><leader>m', function() require('harpoon.mark').add_file() end,
         { desc = 'add file to Harpoon list' })
-    map(This.modes.n, '<leader><leader>H', function() require("util").open_alternate("left") end,
+    map(This.modes.n, '<leader><leader>H', function() require('util').open_alternate('left') end,
         { desc = 'open alternate file left' })
-    map(This.modes.n, '<leader><leader>J', function() require("util").open_alternate("down") end,
+    map(This.modes.n, '<leader><leader>J', function() require('util').open_alternate('down') end,
         { desc = 'open alternate file down' })
-    map(This.modes.n, '<leader><leader>K', function() require("util").open_alternate("up") end,
+    map(This.modes.n, '<leader><leader>K', function() require('util').open_alternate('up') end,
         { desc = 'open alternate file up' })
-    map(This.modes.n, '<leader><leader>L', function() require("util").open_alternate("right") end,
+    map(This.modes.n, '<leader><leader>L', function() require('util').open_alternate('right') end,
         { desc = 'open alternate file right' })
 
     -- FOLLOW REFERENCES --
-    map(This.modes.n, '<leader>]h', function() require("util").open_definition("left") end,
+    map(This.modes.n, '<leader>]h', function() require('util').open_definition('left') end,
         { desc = 'follow reference left' })
-    map(This.modes.n, '<leader>]j', function() require("util").open_definition("down") end,
+    map(This.modes.n, '<leader>]j', function() require('util').open_definition('down') end,
         { desc = 'follow reference down' })
-    map(This.modes.n, '<leader>]k', function() require("util").open_definition("up") end,
+    map(This.modes.n, '<leader>]k', function() require('util').open_definition('up') end,
         { desc = 'follow reference up' })
-    map(This.modes.n, '<leader>]l', function() require("util").open_definition("right") end,
+    map(This.modes.n, '<leader>]l', function() require('util').open_definition('right') end,
         { desc = 'follow reference right' })
 
     -- TOGGLES --
-    map(This.modes.n, '<leader>t2', function() require("util").set_buf_indent(2, false, true) end,
+    map(This.modes.n, '<leader>t2', function() require('util').set_buf_indent(2, false, true) end,
         { desc = 'toggle indent: 2 spaces' })
-    map(This.modes.n, '<leader>t4', function() require("util").set_buf_indent(4, false, true) end,
+    map(This.modes.n, '<leader>t4', function() require('util').set_buf_indent(4, false, true) end,
         { desc = 'toggle indent: 4 spaces' })
-    map(This.modes.n, '<leader>t8', function() require("util").set_buf_indent(8, false, true) end,
+    map(This.modes.n, '<leader>t8', function() require('util').set_buf_indent(8, false, true) end,
         { desc = 'toggle indent: 8 spaces' })
-    map(This.modes.n, '<leader>t<tab>', function() require("util").set_buf_indent(4, true, true) end,
+    map(This.modes.n, '<leader>t<tab>', function() require('util').set_buf_indent(4, true, true) end,
         { desc = 'toggle indent: tabs' })
     map(This.modes.n, '<leader>tl', '<cmd>set list! list?<CR>', { desc = 'toggle visible spaces' })
     map(This.modes.n, '<leader>ts', '<cmd>exec "set scrolloff=" . (102 - &scrolloff)<CR>',
@@ -154,9 +154,9 @@ local function define_mappings()
     map(This.modes.n, '<leader>bx', '<cmd>bufdo bdelete<CR>', { desc = 'close all buffers' })
 
     -- EXECUTING THINGS --
-    map(This.modes.n, '<leader>xl', function() require("util").linkify() end,
+    map(This.modes.n, '<leader>xl', function() require('util').linkify() end,
         { desc = 'linkify', silent = true })
-    map(This.modes.n, '<leader>xn', function() require("util").show_full_path() end,
+    map(This.modes.n, '<leader>xn', function() require('util').show_full_path() end,
         { desc = 'show full path', silent = true })
 
     -- FINDING --
@@ -169,10 +169,10 @@ local function define_mappings()
     map(This.modes.n, '<leader>fi', '<cmd>Telescope treesitter<CR>', { desc = 'find treesitter identifiers' })
     map(This.modes.n, '<leader>fn', '<cmd>NvimTreeFindFileToggle<CR>', { desc = 'open file tree' })
     map(This.modes.n, '<leader>fg',
-        function() require("telescope.builtin").grep_string({ search = vim.fn.input("Grep ❯ ") }) end,
+        function() require('telescope.builtin').grep_string({ search = vim.fn.input('Grep ❯ ') }) end,
         { desc = 'grep in workspace' })
     map(This.modes.n, '<leader>f*',
-        function() require("telescope.builtin").grep_string({ search = vim.fn.expand("<cword>") }) end,
+        function() require('telescope.builtin').grep_string({ search = vim.fn.expand('<cword>') }) end,
         { desc = 'grep current wordt in workspace' })
     map(This.modes.n, '<leader>f:', '<cmd>Telescope commands<CR>', { desc = 'find Vim command' })
     map(This.modes.n, '<leader>fm', '<cmd>Telescope keymaps<CR>', { desc = 'find Vim mapping' })
@@ -265,25 +265,25 @@ end
 -- DAP MAPPINGS --
 function This.setup_dap(bufnr)
     -- DEBUGGING --
-    map(This.modes.n, '<leader>d<space>', function() require("dap").repl.toggle() end,
+    map(This.modes.n, '<leader>d<space>', function() require('dap').repl.toggle() end,
         { buffer = bufnr, desc = 'debug: toggle repl' })
-    map(This.modes.n, '<leader>db', function() require("dap").toggle_breakpoint() end,
+    map(This.modes.n, '<leader>db', function() require('dap').toggle_breakpoint() end,
         { buffer = bufnr, desc = 'debug: toggle breakpoint' })
-    map(This.modes.n, '<leader>dc', function() require("dap").continue() end,
+    map(This.modes.n, '<leader>dc', function() require('dap').continue() end,
         { buffer = bufnr, desc = 'debug: continue' })
-    map(This.modes.n, '<leader>di', function() require("dap").step_into() end,
+    map(This.modes.n, '<leader>di', function() require('dap').step_into() end,
         { buffer = bufnr, desc = 'debug: step into' })
-    map(This.modes.n, '<leader>dl', function() require("dap").run_last() end,
+    map(This.modes.n, '<leader>dl', function() require('dap').run_last() end,
         { buffer = bufnr, desc = 'debug: run last' })
-    map(This.modes.n, '<leader>do', function() require("dap").step_over() end,
+    map(This.modes.n, '<leader>do', function() require('dap').step_over() end,
         { buffer = bufnr, desc = 'debug: step over' })
-    map(This.modes.n, '<leader>dx', function() require("dap").step_out() end,
+    map(This.modes.n, '<leader>dx', function() require('dap').step_out() end,
         { buffer = bufnr, desc = 'debug: step out' })
 
     -- SHOWING THINGS --
-    map(This.modes.n, '<leader>sv', function() require("dap.ui.widgets").hover() end,
+    map(This.modes.n, '<leader>sv', function() require('dap.ui.widgets').hover() end,
         { buffer = bufnr, desc = 'debug: show value' })
-    map(This.modes.v, '<leader>sv', function() require("dap.ui.variables").visual_hover() end,
+    map(This.modes.v, '<leader>sv', function() require('dap.ui.variables').visual_hover() end,
         { buffer = bufnr, desc = 'debug: show value' })
 end
 
