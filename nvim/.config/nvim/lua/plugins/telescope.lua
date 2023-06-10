39835d7df754d71e7b225289be95ed1c64da870d
Jan Ouwens <jan.ouwens@gmail.com>
2023-06-10 10:32:18 +0200

    Neovim: updates Telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
+    tag = '0.1.2',
     dependencies = {
         'nvim-lua/plenary.nvim',
         'kyazdani42/nvim-web-devicons'
