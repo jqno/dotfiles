f8af9e4a642fe2e2b4d44ac8bd2cdaee26e3f466
Jan Ouwens <jan.ouwens@gmail.com>
2023-07-05 08:43:37 +0200

    Neovim: updates auto-save plugin

--- nvim/.config/nvim/lua/plugins/autosave.lua
+++ nvim/.config/nvim/lua/plugins/autosave.lua
@@ -2,19 +2,29 @@ return {
     'okuuva/auto-save.nvim',
     event = 'BufRead', -- load lazily to ensure the splash screen appears
 
-    opts = {
-        execution_message = {
-            dim = 0.4,
-            cleaning_interval = 1000
-        },
-        callbacks = {
-            before_saving = function()
-                local clients = vim.lsp.get_active_clients({ bufnr = 0 })
-                if #clients > 0 and not vim.g.do_not_autoformat and vim.bo.modifiable then
-                    print('smurf')
-                    vim.lsp.buf.format()
+    config = function()
+        require('auto-save').setup({
+            execution_message = {
+                dim = 0.5,
+                cleaning_interval = 1000
+            },
+            trigger_events = {
+                defer_save = { 'CursorHold' }
+            }
+        })
+
+        vim.api.nvim_create_autocmd('User', {
+            group = vim.api.nvim_create_augroup('AutoFormatOnAutoSave', { clear = true }),
+            pattern = 'AutoSaveWritePre',
+            callback = function(opts)
+                local bufnr = opts.data.saved_buffer
+                if bufnr ~= nil then
+                    local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
+                    if #clients > 0 and not vim.g.do_not_autoformat and vim.api.nvim_buf_get_option(bufnr, 'modifiable') then
+                        vim.lsp.buf.format({ bufnr = bufnr })
+                    end
                 end
             end
-        }
-    }
+        })
+    end
 }
