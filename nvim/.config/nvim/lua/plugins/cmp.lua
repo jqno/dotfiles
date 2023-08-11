e58386244fc6b47d50c97930abb689d3bb6b14c1
Jan Ouwens <jan.ouwens@gmail.com>
2023-08-11 13:57:32 +0200

    Neovim: draw pretty borders around cmp completion popup

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -47,6 +47,10 @@ return {
                     require('luasnip').lsp_expand(args.body)
                 end
             },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered()
+            },
             mapping = {
                 ['<Tab>'] = cmp.mapping(tab_complete, { 'i', 's' }),
                 ['<S-Tab>'] = cmp.mapping(s_tab_complete, { 'i', 's' }),
