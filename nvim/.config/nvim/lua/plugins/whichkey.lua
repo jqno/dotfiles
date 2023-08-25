ddb6164d1969336651657d615a103ae7e0250ea9
Jan Ouwens <jan.ouwens@gmail.com>
2023-08-25 13:20:02 +0200

    Neovim: re-adopts whichkey

---
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,35 @@
+return {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 1000
+    end,
+    config = function()
+        local wk = require('which-key')
+        wk.setup({
+            window = {
+                border = 'single'
+            },
+            triggers_blacklist = {
+                n = { 'c', 'v' } -- To avoid conflict with tagalong.vim plugin, which remaps these keys in certain file types
+            }
+        })
+        wk.register({
+            ['<leader>'] = {
+                [']'] = 'Follow references',
+                ['<leader>'] = 'Navigation',
+                b = 'Buffer',
+                f = 'Find',
+                g = 'Go',
+                h = 'History',
+                m = 'Make',
+                r = 'Refactor',
+                s = 'Show',
+                t = 'Toggle',
+                w = 'Window',
+                x = 'Execute'
+            }
+        })
+    end
+}
