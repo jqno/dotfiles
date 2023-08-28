66d3ef48e6837a6d203c92fa2e7d0a1f510d95f8
Jan Ouwens <jan.ouwens@gmail.com>
2023-08-28 09:11:13 +0200

    Neovim: replaces vim-sandwich with nvim-surround

--- nvim/.config/nvim/lua/plugins/sandwich.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'machakann/vim-sandwich',
-
-    config = function()
-        vim.cmd('runtime macros/sandwich/keymap/surround.vim')
-    end
-}
---
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,7 @@
+return {
+    'kylechui/nvim-surround',
+    version = '*',
+    event = 'VeryLazy',
+
+    opts = {}
+}
