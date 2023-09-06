e46c80fbfc3086af3ebf9cf4466b9553294789b5
Jan Ouwens <jan.ouwens@gmail.com>
2023-09-06 12:14:07 +0200

    Neovim: autoformat on BufLeave and FocusLost, instead of on Save

--- nvim/.config/nvim/lua/plugins/autosave.lua
+++ nvim/.config/nvim/lua/plugins/autosave.lua
@@ -12,19 +12,5 @@ return {
                 defer_save = { 'CursorHold' }
             }
         })
-
-        vim.api.nvim_create_autocmd('User', {
-            group = vim.api.nvim_create_augroup('AutoFormatOnAutoSave', { clear = true }),
-            pattern = 'AutoSaveWritePre',
-            callback = function(opts)
-                local bufnr = opts.data.saved_buffer
-                if bufnr ~= nil then
-                    local clients = vim.lsp.get_active_clients({ bufnr = bufnr })
-                    if #clients > 0 and not vim.g.do_not_autoformat and vim.api.nvim_buf_get_option(bufnr, 'modifiable') then
-                        vim.lsp.buf.format({ bufnr = bufnr })
-                    end
-                end
-            end
-        })
     end
 }
