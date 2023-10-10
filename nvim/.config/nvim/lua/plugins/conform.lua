42328f206fa7367428972353ef161b88fd8b46f3
Jan Ouwens <jan.ouwens@gmail.com>
2023-10-10 10:35:23 +0200

    Software/Neovim: updates prettier-java

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -4,13 +4,17 @@ return {
 
     opts = {
         formatters_by_ft = {
-            java = { 'prettier' },
+            java = { 'prettier_java' },
             markdown = { 'prettier' },
             sh = { 'shellharden' },
             sql = { 'configured_sql_formatter' },
             ['_'] = { 'trim_whitespace' }
         },
         formatters = {
+            prettier_java = {
+                command = 'prettier',
+                args = { '--stdin-filepath', '--plugin=/home/linuxbrew/.linuxbrew/lib/node_modules/prettier-plugin-java/dist/index.js', '$FILENAME' }
+            },
             configured_sql_formatter = {
                 command = 'sql-formatter',
                 args = { '--config', vim.env.HOME .. '/.sql-formatter.json', '$FILENAME' }
