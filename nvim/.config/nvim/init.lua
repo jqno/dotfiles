a77c60633fad733f9d6425073f7f1e0118c1ea89
Jan Ouwens <jan.ouwens@gmail.com>
2023-10-10 14:21:44 +0200

    Neovim: optionally load local configuration

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,3 +4,9 @@ require('config.mappings').setup()
 require('config.commands').setup()
 require('config.colorscheme').setup()
 require('config.lsp').setup()
+
+-- Optionally require ~/.nvim-local.lua
+local local_conf = vim.env.HOME .. '/.nvim-local.lua'
+if vim.fn.filereadable(local_conf) == 1 then
+    vim.cmd('luafile ' .. local_conf)
+end
