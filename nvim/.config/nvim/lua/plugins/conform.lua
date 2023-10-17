8bb1838268c7ffdb349dfb59a0f2a26ef0d39ea1
Jan Ouwens <jan.ouwens@gmail.com>
2023-10-17 10:02:37 +0200

    Neovim: re-reconfigures autoformatting (not on autosave but only when leaving the split)

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,23 +2,37 @@ return {
     'stevearc/conform.nvim',
     event = 'UIEnter',
 
-    opts = {
-        formatters_by_ft = {
-            java = { 'prettier_java' },
-            markdown = { 'prettier' },
-            sh = { 'shellharden' },
-            sql = { 'configured_sql_formatter' },
-            ['_'] = { 'trim_whitespace' }
-        },
-        formatters = {
-            prettier_java = {
-                command = 'prettier',
-                args = { '--stdin-filepath', '--plugin=/home/linuxbrew/.linuxbrew/lib/node_modules/prettier-plugin-java/dist/index.js', '$FILENAME' }
+    config = function()
+        require('conform').setup({
+            formatters_by_ft = {
+                java = { 'prettier_java' },
+                markdown = { 'prettier' },
+                sh = { 'shellharden' },
+                sql = { 'configured_sql_formatter' },
+                ['_'] = { 'trim_whitespace' }
             },
-            configured_sql_formatter = {
-                command = 'sql-formatter',
-                args = { '--config', vim.env.HOME .. '/.sql-formatter.json', '$FILENAME' }
+            formatters = {
+                prettier_java = {
+                    command = 'prettier',
+                    args = { '--stdin-filepath',
+                        '--plugin=/home/linuxbrew/.linuxbrew/lib/node_modules/prettier-plugin-java/dist/index.js',
+                        '$FILENAME' }
+                },
+                configured_sql_formatter = {
+                    command = 'sql-formatter',
+                    args = { '--config', vim.env.HOME .. '/.sql-formatter.json', '$FILENAME' }
+                }
             }
-        }
-    }
+        })
+
+        vim.api.nvim_create_autocmd({ 'FocusLost', 'WinLeave' }, {
+            group = vim.api.nvim_create_augroup('AutoFormat', { clear = true }),
+            pattern = '*',
+            callback = function(args)
+                if args.buf ~= nil and (vim.g.do_autoformat or vim.b[args.buf].do_autoformat) then
+                    require('conform').format({ bufnr = args.buf, async = true, lsp_fallback = true })
+                end
+            end
+        })
+    end
 }
