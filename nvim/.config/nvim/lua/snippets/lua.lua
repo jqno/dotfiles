2c3b3856c9acb25de00439e053c6425adad9cfe4
Jan Ouwens <jan.ouwens@gmail.com>
2023-10-20 19:16:27 +0200

    Neovim: adds Lua snippet

--- nvim/.config/nvim/lua/snippets/lua.lua
+++ nvim/.config/nvim/lua/snippets/lua.lua
@@ -2,6 +2,16 @@ local ls = require('luasnip')
 local parse = ls.parser.parse_snippet
 
 return {
+    parse({ trig = 'start', dscr = 'Start a new file' },
+        [[
+        local This = {}
+
+        $1
+
+        return This
+        ]]
+    ),
+
     parse({ trig = 'parse', dscr = 'Snippet parser' },
         [==[
         parse({ trig = '$1', dscr = '$2' },
