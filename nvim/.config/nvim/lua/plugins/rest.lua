102cf750984d53c11f274fc322c36c9a664f843a
Jan Ouwens <jan.ouwens@gmail.com>
2023-11-06 14:58:33 +0100

    Neovim: adds REST request plugin

---
+++ nvim/.config/nvim/lua/plugins/rest.lua
@@ -0,0 +1,11 @@
+return {
+    'rest-nvim/rest.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim'
+    },
+    ft = { 'http' },
+
+    opts = {
+        result_split_horizontal = true
+    }
+}
