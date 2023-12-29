67f5ae9b3df121edf02e18aacab8322088a04a8c
Jan Ouwens <jan.ouwens@gmail.com>
2023-12-29 10:31:12 +0100

    Neovim: simplifies colorscheme config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,7 +2,6 @@ require('config.settings').setup()
 require('config.lazy').setup()
 require('config.mappings').setup()
 require('config.commands').setup()
-require('config.colorscheme').setup()
 require('config.lsp').setup()
 
 -- Optionally require ~/.nvim-local.lua
--- nvim/.config/nvim/lua/plugins/tranquility.lua
+++ nvim/.config/nvim/lua/plugins/tranquility.lua
@@ -4,5 +4,10 @@ return {
 
     config = function()
         vim.g.tranquility_overrides = { invert_statusline = true }
+        vim.cmd.colorscheme('green-tranquility')
+
+        -- Fix square in the corner between vertical splits
+        local hl_statusline = vim.api.nvim_get_hl(0, { name = 'StatusLine', link = true })
+        vim.api.nvim_set_hl(0, "StatusLineNC", { bg = hl_statusline.background })
     end
 }
