b0394f77eb6a745815bbda8073712d0d6abb09f7
Jan Ouwens <jan.ouwens@gmail.com>
2024-01-24 16:09:54 +0100

    Neovim: reduce severity of Vale linter

--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -1,9 +1,23 @@
+local function reduce_vale_severity()
+    local lint = require('lint')
+    local original_parser = lint.linters.vale.parser
+    local reduced_parser = function(output, bufnr, linter_cwd)
+        local issues = original_parser(output, bufnr, linter_cwd)
+        for _, entry in ipairs(issues) do
+            entry.severity = vim.diagnostic.severity.INFO
+        end
+        return issues
+    end
+    lint.linters.vale.parser = reduced_parser
+end
+
 return {
     'mfussenegger/nvim-lint',
     event = 'BufReadPre',
 
     config = function()
         local lint = require('lint')
+        reduce_vale_severity()
 
         -- Configure linters
         lint.linters_by_ft = {
