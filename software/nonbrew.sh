bae3e9a2949c8eabdf24986071a8c6856853163d
Jan Ouwens <jan.ouwens@gmail.com>
2024-01-24 10:25:08 +0100

    Ulauncher: removes snippets extension, which crashes all the time

--- software/nonbrew.sh
+++ software/nonbrew.sh
@@ -5,6 +5,3 @@ npm install -g @fsouza/prettierd
 npm install --prefix "$(npm root -g)"/@fsouza/prettierd prettier-plugin-java
 
 /usr/bin/pip3 install requests
-
-# For ulauncher
-/usr/bin/pip3 install --user dateparser jinja2 python-frontmatter markdown
