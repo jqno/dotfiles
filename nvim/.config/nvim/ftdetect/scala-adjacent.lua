0ac4cde975af612ccd8b0ff6869bfc3c2ad51427
Jan Ouwens <jan.ouwens@gmail.com>
2024-02-06 14:21:18 +0100

    Neovim: detect SBT files

--- nvim/.config/nvim/ftdetect/scala-adjacent.lua
+++ nvim/.config/nvim/ftdetect/scala-adjacent.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-    group = vim.api.nvim_create_augroup('FileTypeWorksheetSc', {}),
-    pattern = '*.worksheet.sc',
+    group = vim.api.nvim_create_augroup('FileTypeScalaAdjacent', {}),
+    pattern = { '*.worksheet.sc', '*.sbt' },
     callback = function() vim.opt.filetype = 'scala' end
 })
