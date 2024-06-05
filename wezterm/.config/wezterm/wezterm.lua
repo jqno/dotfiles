81ec4f28db15931fa884aac663eea66975dfb871
Jan Ouwens <jan.ouwens@gmail.com>
2024-06-05 20:10:15 +0200

    WezTerm: adds random background image

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,11 +1,13 @@
 local wezterm = require('wezterm')
 local act = wezterm.action
 local nerdfonts = wezterm.nerdfonts
+local os = require('os')
 
 local config = wezterm.config_builder()
 
 local constants = {
     tab_max_width = 32,
+    wallpaper_location = os.getenv('HOME') .. '/Dropbox/wallpapers',
     tab_title = {
         left_circle = nerdfonts.ple_left_half_circle_thick,
         right_circle = nerdfonts.ple_right_half_circle_thick,
@@ -40,6 +42,24 @@ local constants = {
     }
 }
 
+local function random_wallpaper()
+    local wallpapers = {}
+    local handle = io.popen('/bin/ls -1 ' .. constants.wallpaper_location)
+    if handle then
+        for line in handle:lines() do
+            table.insert(wallpapers, line)
+        end
+        handle:close()
+    end
+
+    if #wallpapers > 0 then
+        local index = math.random(1, #wallpapers)
+        return constants.wallpaper_location .. '/' .. wallpapers[index]
+    end
+    return nil
+end
+
+
 config = {
     -- Appearance
     adjust_window_size_when_changing_font_size = false,
@@ -48,6 +68,10 @@ config = {
     use_fancy_tab_bar = false,
     window_decorations = 'NONE',
     tab_max_width = constants.tab_max_width,
+    inactive_pane_hsb = {
+        saturation = 0.8,
+        brightness = 0.5,
+    },
     font = wezterm.font {
         family = 'monolisa',
         harfbuzz_features = { 'calt', '-liga', 'ss10', 'ss11', 'ss14' }
@@ -96,7 +120,12 @@ config = {
         brights = {
             '#3b3b3b', '#ff5e56', '#83c746', '#efc541', '#4f9cfe', '#ff81ca', '#56d8c9', '#dedede',
         }
-    }
+    },
+
+    -- Wallpaper
+    window_background_image_hsb = {
+        brightness = 0.08
+    },
 }
 
 wezterm.on('format-tab-title', function(tab)
@@ -149,6 +178,19 @@ wezterm.on('format-tab-title', function(tab)
             { Text = c.right_circle },
         }
     end
+end)
+
+local last_hour = 0
+
+wezterm.on('update-right-status', function(win)
+    local now = tonumber(wezterm.strftime('%s')) or 0
+    if now - last_hour >= 3600 then
+        last_hour = now
+        win:set_config_overrides({
+            window_background_image = random_wallpaper(),
+        })
+    end
+    -- win:set_right_status(wezterm.format { { Text = tostring(last_hour) } })
 end)
 
 return config
