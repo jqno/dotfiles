687005b9ab2f015737c66fca5215b56fd38b84ef
Jan Ouwens <jan.ouwens@gmail.com>
2024-06-13 13:50:39 +0200

    Scripts: improves font-rendering-test.sh

--- scripts/scripts/dotfiles/font-rendering-test.sh
+++ scripts/scripts/dotfiles/font-rendering-test.sh
@@ -2,15 +2,16 @@
 
 LIGATURES="-> === != <= www"
 NERDSYMBOLS="        "
+FONTSAMPLE="3g&"
 FONTCONFIG="calt:{| ss11:0xF ss14:&"
-SYMBOLS="$NERDSYMBOLS     $LIGATURES     $FONTCONFIG"
+SYMBOLS="$NERDSYMBOLS     $LIGATURES     $FONTSAMPLE      $FONTCONFIG"
 
 kitty +list-fonts --psnames
 
 echo ""
 echo ""
 echo ""
-echo -e "  Name        Nerd symbols      Ligatures            Font config (MonoLisa)"
+echo -e "  Name        Nerd symbols      Ligatures            Sample   Font config (MonoLisa)"
 echo -e "* Regular     $SYMBOLS"
 echo -e "* \033[1mBold        $SYMBOLS\033[0m"
 echo -e "* \033[3mItalic      $SYMBOLS\033[0m"
