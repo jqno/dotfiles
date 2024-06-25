f62aadc4294cf5f8d0c1f382890859cef13a5f16
Jan Ouwens <jan.ouwens@gmail.com>
2024-06-25 11:50:10 +0200

    Zsh: adds dockerarmageddon alias

--- zsh/.config/zsh/aliases.sh
+++ zsh/.config/zsh/aliases.sh
@@ -63,6 +63,14 @@ dockerstopall() {
   done
 }
 
+function dockerarmageddon {
+  dockerstopall
+  docker network prune -f
+  docker rmi -f $(docker images --filter dangling=true -qa)
+  docker volume rm $(docker volume ls --filter dangling=true -q)
+  docker rmi -f $(docker images -qa)
+}
+
 # mcd
 function mcd() {
   mkdir -p -- "$1" && cd -P -- "$1"
