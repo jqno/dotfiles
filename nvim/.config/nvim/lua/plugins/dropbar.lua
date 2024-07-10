7c32f4dbdc0a4e72c19201ae0734737106e7b914
Jan Ouwens <jan.ouwens@gmail.com>
2024-07-10 07:48:08 +0200

    Neovim: tweaks dropbar to not repeat packages in Scala

--- nvim/.config/nvim/lua/plugins/dropbar.lua
+++ nvim/.config/nvim/lua/plugins/dropbar.lua
@@ -6,5 +6,47 @@ return {
         vim.opt.mousemoveevent = true
         vim.api.nvim_set_hl(0, 'WinBar', { link = 'Normal' })
         vim.api.nvim_set_hl(0, 'WinBarNC', { link = 'Normal' })
+
+        require('dropbar').setup({
+            bar = {
+                sources = function(buf, _)
+                    local sources = require('dropbar.sources')
+                    local utils = require("dropbar.utils")
+                    local filename = {
+                        get_symbols = function(buff, win, cursor)
+                            local paths = sources.path.get_symbols(buff, win, cursor)
+                            return { paths[#paths] }
+                        end,
+                    }
+                    if vim.bo[buf].ft == 'scala' then
+                        return {
+                            filename,
+                            utils.source.fallback({
+                                sources.lsp,
+                                sources.treesitter
+                            })
+                        }
+                    end
+                    if vim.bo[buf].ft == 'markdown' then
+                        return {
+                            sources.path,
+                            sources.markdown,
+                        }
+                    end
+                    if vim.bo[buf].buftype == 'terminal' then
+                        return {
+                            sources.terminal
+                        }
+                    end
+                    return {
+                        sources.path,
+                        utils.source.fallback({
+                            sources.lsp,
+                            sources.treesitter
+                        })
+                    }
+                end
+            }
+        })
     end,
 }
