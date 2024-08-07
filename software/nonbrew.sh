29070d06264f40aceb78bafc83f9333ee737b5d8
Jan Ouwens <jan.ouwens@gmail.com>
2024-08-07 08:37:45 +0200

    Software: locks prettier-plugin-java to a version to avoid issues with prettier as run from maven

--- software/nonbrew.sh
+++ software/nonbrew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-npm install -g prettier prettier-plugin-java
+npm install -g prettier prettier-plugin-java@2.4.0
 npm install -g @fsouza/prettierd
-npm install --prefix "$(npm root -g)"/@fsouza/prettierd prettier-plugin-java
+npm install --prefix "$(npm root -g)"/@fsouza/prettierd prettier-plugin-java@2.4.0
 
 /usr/bin/pip3 install requests
