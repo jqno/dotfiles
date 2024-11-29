98a4c32ef50d511ceef3aa48a00d4c931d1b307d
Jan Ouwens <jan.ouwens@gmail.com>
2024-11-29 15:20:49 +0100

    Neovim: save before running terminal commands

--- nvim/.config/nvim/lua/plugins/floaterm.lua
+++ nvim/.config/nvim/lua/plugins/floaterm.lua
@@ -1,4 +1,5 @@
 local function send(cmd)
+    vim.cmd.write() -- Just in case the auto-save doesn't catch it
     require('plugins.conform').autoformat()
 
     if vim.fn['floaterm#buflist#curr']() == -1 then
