f45adf392e05778b546eadf0658fdce3cf59953f
Jan Ouwens <jan.ouwens@gmail.com>
2025-03-13 11:33:25 +0100

    Neovim: removes render-markdown

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,21 +1,4 @@
-local map = vim.keymap.set
-local modes = require('util.modes')
-
 vim.opt_local.wrap = true
 vim.opt_local.commentstring = '<!-- %s -->'
 
 require('util.autolist').setup()
-
-local id = nil
-id = vim.api.nvim_create_autocmd('InsertLeave', {
-    group = vim.api.nvim_create_augroup('disable_markdown_render', { clear = true }),
-    buffer = 0,
-    callback = function()
-        vim.cmd.RenderMarkdown('disable')
-        if id ~= nil then
-            vim.api.nvim_del_autocmd(id)
-        end
-    end
-})
-
-map(modes.n, '<leader>tr', '<cmd>RenderMarkdown toggle<CR>', { desc = 'toggle Markdown rendering' })
