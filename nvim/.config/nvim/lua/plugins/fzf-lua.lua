a9acf57a6709344aa2eeae1197269c19db06e547
Jan Ouwens <jan.ouwens@gmail.com>
2025-05-09 12:36:06 +0200

    Neovim: introduces fzf-lua

---
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,22 @@
+return {
+    "ibhagwan/fzf-lua",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons"
+    },
+    event = 'UIEnter',
+
+    config = function()
+        local fzflua = require('fzf-lua')
+        fzflua.setup({
+            'telescope',
+            keymap = {
+                fzf = {
+                    ['ctrl-a'] = 'toggle-all'
+                }
+            },
+            files = {
+                formatter = "path.filename_first",
+            }
+        })
+    end
+}
