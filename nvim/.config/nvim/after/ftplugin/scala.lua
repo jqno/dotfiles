614c59d8d8c42f2f004f7209dba8ec667c6c7099
Jan Ouwens <jan.ouwens@gmail.com>
2025-12-16 16:37:06 +0100

    Neovim: speeds up Scala SBT invocation

--- nvim/.config/nvim/after/ftplugin/scala.lua
+++ nvim/.config/nvim/after/ftplugin/scala.lua
@@ -3,6 +3,8 @@ local modes = require('util.modes')
 local mappings = require('config.mappings')
 local terminal = require('util.terminal')
 
+vim.g['test#scala#sbttest#executable'] = 'sbt --client'
+
 vim.opt_local.formatoptions:remove('o')
 
 map(modes.n, '<leader>m<space>',
@@ -11,13 +13,13 @@ map(modes.n, '<leader>m<space>',
 
 map(modes.n, '<leader>ro', '<cmd>MetalsOrganizeImports<CR>', { buffer = true, desc = 'refactor: organize imports' })
 map(modes.n, '<leader>mr', '<cmd>MetalsCompileClean<CR>', { buffer = true, desc = 'Metals clean compile' })
-map(modes.n, '<leader>mCC', function() terminal.send('sbt clean Test/compile') end,
+map(modes.n, '<leader>mCC', function() terminal.send('sbt --client clean Test/compile') end,
     { buffer = true, desc = 'sbt clean compile' })
-map(modes.n, '<leader>mcc', function() terminal.send('sbt Test/compile') end,
+map(modes.n, '<leader>mcc', function() terminal.send('sbt --client Test/compile') end,
     { buffer = true, desc = 'sbt compile' })
-map(modes.n, '<leader>mv', function() terminal.send('sbt compile scalafmtCheckAll testOnlyUnit') end,
+map(modes.n, '<leader>mv', function() terminal.send('sbt --client compile scalafmtCheckAll testOnlyUnit') end,
     { buffer = true, desc = 'sbt verify' })
-map(modes.n, '<leader>mCv', function() terminal.send('sbt clean compile scalafmtCheckAll test') end,
+map(modes.n, '<leader>mCv', function() terminal.send('sbt --client clean compile scalafmtCheckAll test') end,
     { buffer = true, desc = 'sbt verify' })
 mappings.setup_dap(vim.api.nvim_get_current_buf())
 
