4215a9fabc829d5d935caa8f80246f1d34eec78f
Jan Ouwens <jan.ouwens@gmail.com>
2026-02-09 11:32:22 +0100

    Metals: pin version so :MetalsUpdate doesn't accidentally install Milestone versions that don't work

--- nvim/.config/nvim/lua/plugins/metals.lua
+++ nvim/.config/nvim/lua/plugins/metals.lua
@@ -5,6 +5,7 @@ local function metals_config(capabilities)
         statusBarProvider = 'off'
     }
     cfg.settings = {
+        serverVersion = '1.6.5',
         defaultBspToBuildTool = true,
         enableBestEffort = true,
         scalafixConfigPath = vim.env.HOME .. '/.scalafix.conf',
