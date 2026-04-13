bb32ec762750b8faeabb84c57d518682ccd20814
Jan Ouwens <jan.ouwens@gmail.com>
2026-04-13 12:54:39 +0200

    Neovim: fixes treesitter configuration

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,41 +2,27 @@ return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     build = ':TSUpdate',
+    branch = 'main',
     dependencies = {
         'nvim-treesitter/nvim-treesitter-textobjects',
         'RRethy/nvim-treesitter-endwise'
     },
 
-    config = function()
-        require('nvim-treesitter').setup({
-            ensure_installed = {
-                'comment', 'diff',
-                'bash', 'css', 'hocon', 'html', 'http', 'java', 'javascript', 'json',
-                'kotlin', 'lua', 'make', 'markdown', 'nix', 'python', 'regex',
-                'ruby', 'rust', 'scala', 'scss', 'sql', 'typescript', 'vim', 'vimdoc',
-                'xml', 'yaml'
-            },
-            highlight = {
-                enable = true
-            },
-            textobjects = {
-                select = { enable = true, keymaps = { ['if'] = '@call.outer' } },
-                move = {
-                    enable = true,
-                    goto_next_start = { [']]'] = '@function.outer' },
-                    goto_previous_start = { ['[['] = '@function.outer' }
-                },
-                lsp_interop = {
-                    enable = true,
-                    peek_definition_code = {
-                        ['<leader>sc'] = '@class.outer',
-                        ['<leader>sf'] = '@function.outer'
-                    }
-                }
-            },
-            endwise = {
-                enable = true
-            }
+    init = function()
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                -- Enable treesitter highlighting and disable regex syntax
+                pcall(vim.treesitter.start)
+                -- Enable treesitter-based indentation
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+        require('nvim-treesitter').install({
+            'comment', 'diff',
+            'bash', 'css', 'hocon', 'html', 'http', 'java', 'javascript', 'json',
+            'kotlin', 'lua', 'make', 'markdown', 'nix', 'python', 'regex',
+            'ruby', 'rust', 'scala', 'scss', 'sql', 'typescript', 'vim', 'vimdoc',
+            'xml', 'yaml'
         })
     end
 }
